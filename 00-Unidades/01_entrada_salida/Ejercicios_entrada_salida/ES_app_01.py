aef9280c11a86b2e07e4b8efcf36bb865b9190a8
agustin <haramoglis.agustin@gmail.com>
2024-02-06 12:30:27 -0300

    ejercicios resueltos

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_01.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_01.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: Agustin
+apellido: Haramoglis
 ---
 Ejercicio: entrada_salida_01
 ---
@@ -26,7 +26,7 @@ class App(customtkinter.CTk):
         self.btn_mostrar.grid(row=2, pady=20, columnspan=2, sticky="nsew")
 
     def btn_mostrar_on_click(self):
-        alert("titulo", "Esto no anda funciona")
+        alert("Alerta", "Esto no anda funciona")
 
 
 if __name__ == "__main__":
--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_02.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_02.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: Agustin
+apellido: Haramoglis
 ---
 Ejercicio: entrada_salida_02
 ---
