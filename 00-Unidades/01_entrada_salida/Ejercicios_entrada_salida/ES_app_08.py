a83eeb8f618e761189b64eddb8998ff04b291c2a
agustin <haramoglis.agustin@gmail.com>
2024-02-08 09:20:26 -0300

    ejercicios 7 y 8

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_08.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_08.py
@@ -40,9 +40,15 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        operador_a = int(self.txt_operador_a.get())
+        operador_b = int(self.txt_operador_b.get())
         
-        
+        operador_a = int(operador_a)
+        operador_b = int(operador_b)
+
+        resultado= operador_a % operador_b
+
+        alert("Datos", f"el resultado del resto de {operador_a} y {operador_b} es: {resultado}")
     
 if __name__ == "__main__":
     app = App()
